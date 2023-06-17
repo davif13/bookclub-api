2814ed4f030ed90af02d2e42c717619273dd0eee
davif13 <davi.ffa@gmail.com>
2023-06-17 12:54:03 -0300

    feat: add userbook model and migrations

--- src/models/book.js
+++ src/models/book.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: "category_id",
         as: "category",
       });
+      this.hasMany(models.UserBook, { foreignKey: "book_id", as: "book" });
     }
   }
   Book.init(
