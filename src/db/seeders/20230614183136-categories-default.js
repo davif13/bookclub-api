85c910c1b542777a53c50307b932c38a2cf47e13
davif13 <davi.ffa@gmail.com>
2023-06-19 18:49:09 -0300

    feat: add bio to authors and created seeds

--- src/db/seeders/20230614183136-categories-default.js
+++ src/db/seeders/20230614183136-categories-default.js
@@ -7,54 +7,63 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Categories", [
       {
+        id: 1,
         name: "Ficção",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 2,
         name: "Negócios",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 3,
         name: "Autoajuda",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 4,
         name: "Romance",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 5,
         name: "HQs e Mangás",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 6,
         name: "Culinária",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 7,
         name: "Computação",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 8,
         name: "Saúde",
         highlighted: false,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
+        id: 9,
         name: "Biografia",
         highlighted: false,
         createdAt: new Date(),
@@ -64,11 +73,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete("Categories", null, {});
   },
 };
